07deea94c868277aec14404aa3ace18d470b81c3
Alikhan219 <merabhai786786@gmail.com>
2021-01-19 05:46:07 -0800

    clearing the video error

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React from "react"
-
+import video from "./make.mp4"
 const LandingPage=()=>{
     return(
-        <div>
+        
   <div className="header-container">
-  <video src=""> 
+  <video autoPlay muted loop="true" src={video}>
+     
      </video>
   </div>
-        </div>
     )
 }
 
