5b30c44afce4deb5f86a5ea9522b6e0fce707246
Alikhan219 <merabhai786786@gmail.com>
2021-01-20 10:56:45 -0800

    adding the new div for the text overlay

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,11 +2,15 @@ import React from "react"
 import video from "./make.mp4"
 const LandingPage=()=>{
     return(
-        
   <div className="header-container">
+  <div className="full_screen_wrap">
   <video autoPlay muted loop="true" src={video}>
-     
      </video>
+     </div>
+     <div className="header-overlay"></div>
+     <div className="header-div">
+     <h1>Welcome To The Islamic Science</h1>
+     </div>
   </div>
     )
 }
