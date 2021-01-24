5b2412777b3a04ea9e624b913b38835035a4442e
Alikhan219 <merabhai786786@gmail.com>
2021-01-24 07:46:23 -0800

    creating new component

---
+++ src/Products.jsx
@@ -0,0 +1,15 @@
+
+import React from "react"
+
+
+
+const Produts= ()=>{
+    return(
+        <>
+           <div>
+               
+           </div>
+        </>
+    )
+}
+export default Products;
