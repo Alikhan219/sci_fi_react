f7aa097c90c0b58fb3b08fb01961d3fdcb67e97d
Alikhan219 <merabhai786786@gmail.com>
2021-01-25 07:31:42 -0800

    creating new div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import LandingPage from "./LandingPage"
+import Products from "./Products"
 function App() {
   return (
     <div className="App">
      <LandingPage />
+     <Products />
     </div>
   );
 }
--- src/Products.jsx
+++ src/Products.jsx
@@ -3,11 +3,19 @@ import React from "react"
 
 
 
-const Produts= ()=>{
+const Products= ()=>{
     return(
         <>
            <div>
-               
+               <div>
+        <div>
+            <img src="" alt="" />
+            <h3>Ibn e Sina</h3>
+            <p>Scientist</p>
+        </div>
+
+               </div>
+
            </div>
         </>
     )
