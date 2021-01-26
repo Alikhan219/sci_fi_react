e84f36c88eabffd9f8a9f0e588fbb70eca509fec
Alikhan219 <merabhai786786@gmail.com>
2021-01-26 07:53:59 -0800

    creating cards

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,23 +1,35 @@
+import React from "react";
 
-import React from "react"
-
-
-
-const Products= ()=>{
-    return(
-        <>
-           <div>
-               <div>
-        <div>
-            <img src="" alt="" />
-            <h3>Ibn e Sina</h3>
-            <p>Scientist</p>
+const Products = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="card">
+          <img src="https://i.pinimg.com/564x/12/ca/c0/12cac01234f3f23aa1a8a26c48feb4e8.jpg" alt="" />
+          <div className="card-txt">
+   <h1>IBN E SINA</h1>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa minima praesentium at?</p>
+   <a href="" className="btn-1">Read</a>
+          </div>
         </div>
-
-               </div>
-
-           </div>
-        </>
-    )
-}
+        <div className="card">
+          <img src="https://i.pinimg.com/originals/c6/9c/b7/c69cb7aae84ab0e87da521cd3fa8e145.jpg?epik=dj0yJnU9Zm9RRXdqLWlaTVZHX1JpRGdPSnUxUTFuWWl4LXNRT0smcD0wJm49aWJKSFRSUXljMGRlNkFjY3RKQ0x6USZ0PUFBQUFBR0FRT3Fn" alt="" />
+          <div className="card-txt">
+   <h1>JABIR IBN E HAYYAN</h1>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa minima praesentium at?</p>
+   <a href="" className="btn-2">Read</a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="https://i.pinimg.com/564x/12/ca/c0/12cac01234f3f23aa1a8a26c48feb4e8.jpg" alt="" />
+          <div className="card-txt">
+   <h1>IBN E SINA</h1>
+   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa minima praesentium at?</p>
+   <a href="" className="btn-3">Read</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Products;
